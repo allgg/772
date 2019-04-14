8315dea3feb1b0353ed3860363fe70bdcf65da90
allgg <49602399+allgg@users.noreply.github.com>
2019-04-14 11:56:00 +0800

    Update xs-top.js

--- xs-top.js
+++ xs-top.js
@@ -1,2 +1,1 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-document.writeln("1");
