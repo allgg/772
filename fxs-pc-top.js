07fc06c82111b2b15b8de9e8ab0195e064632362
allgg <49602399+allgg@users.noreply.github.com>
2019-04-14 16:35:46 +0800

    Update fxs-pc-top.js

--- fxs-pc-top.js
+++ fxs-pc-top.js
@@ -1,1 +1,7 @@
-document.write("fxs-pc-top")
+document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<script>");
+document.writeln("     (adsbygoogle = window.adsbygoogle || []).push({");
+document.writeln("          google_ad_client: \'ca-pub-6866243199418415\',");
+document.writeln("          enable_page_level_ads: true");
+document.writeln("     });");
+document.writeln("</script>");
