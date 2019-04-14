4c0639ebf1fb0f9c5a5f003f86d90c0a72f0476d
allgg <49602399+allgg@users.noreply.github.com>
2019-04-14 11:08:44 +0800

    Update 772top.js

--- 772top.js
+++ 772top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://www.20080088.com/ygzd\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3043/b8af7ce3-380b-493f-be2e-ad392ea0aa68.gif\'></a>");
+//document.writeln("<a href=\'https://www.20080088.com/ygzd\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3043/b8af7ce3-380b-493f-be2e-ad392ea0aa68.gif\'></a>");
