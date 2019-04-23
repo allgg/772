6e311248322c90841c5901f7f3896011267a2dea
allgg <49602399+allgg@users.noreply.github.com>
2019-04-23 17:19:52 +0800

    Update xs-foot.js

--- xs-foot.js
+++ xs-foot.js
@@ -1,9 +1,14 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-document.writeln("<a href=\'https://m.zhyecheng.com/ua80512/  \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
-var s = document.createElement('script');
+document.writeln("<a href=\'https://m.kaisadadigood.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+  var s = document.createElement('script');
     s.type = 'text/javascript';
     s.async = true;
-    s.src = 'https://anthonython.com:9595/v/104_150/1926_1';
+    s.src = 'https://christinehris.com:9595/v/104_150/1926_1';
     var head = document.getElementsByTagName('head')[0];
-    head.appendChild(s);                                                                 
-                                           
+    head.appendChild(s);   
+ var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://christinehris.com:9595/v/105/1926_1';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);  
