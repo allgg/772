f948b2dfdab0517eca233d75c480bd7b4269326e
allgg <49602399+allgg@users.noreply.github.com>
2019-04-30 13:13:42 +0800

    Update zjd-pc-top.js

--- zjd-pc-top.js
+++ zjd-pc-top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://m.kaisadadigood.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'https://m.kaisadadigood.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\'></a>");
