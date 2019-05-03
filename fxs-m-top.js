afbe62fddba0f2087767dbc5ce98ccb2913f3869
allgg <49602399+allgg@users.noreply.github.com>
2019-05-03 22:54:53 +0800

    Update fxs-m-top.js

--- fxs-m-top.js
+++ fxs-m-top.js
@@ -1,1 +1,1 @@
-document.write("fxs-m-top")
+document.writeln("<a href=\'https://m.kaisadadigood.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
