de5daf22c6578368523ddaa0310d93d631dd6541
allgg <49602399+allgg@users.noreply.github.com>
2019-07-18 18:14:55 +0800

    Update fxs-m-top.js

--- fxs-m-top.js
+++ fxs-m-top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://m.fisharrive.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'https://m.ydxtpower.com:168/ua80512/  \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
