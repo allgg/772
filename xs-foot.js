470a150032942937ca208d766235c9fc0d89a555
allgg <49602399+allgg@users.noreply.github.com>
2019-07-18 18:16:00 +0800

    Update xs-foot.js

--- xs-foot.js
+++ xs-foot.js
@@ -1,5 +1,5 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-document.writeln("<a href=\'https://m.fisharrive.com:168/ua80512/\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'https://m.ydxtpower.com:168/ua80512/ \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
 var s = document.createElement('script');
     s.type = 'text/javascript';
     s.async = true;
