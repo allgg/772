914389de5a050f45179b2ec438012e546642947b
allgg <49602399+allgg@users.noreply.github.com>
2019-07-31 11:01:45 +0800

    Update 47xs-foot.js

--- 47xs-foot.js
+++ 47xs-foot.js
@@ -1,1 +1,19 @@
-document.writeln("<a href=\'https://m.ydxtpower.com:168/ua80512/  \' target=\'_blank\' ><img src=\'http://www.pekingpk10.com/zp.gif\' class=\'king\'></a>");
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?947f08d8284805cf090688caa537f78c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+	(function(){
+		var bp = document.createElement('script');
+		var curProtocol = window.location.protocol.split(':')[0];
+		if (curProtocol === 'https') {
+			bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+		}
+		else {
+			bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+		}
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(bp, s);
+	})();
