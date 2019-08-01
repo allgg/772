4c814bfbe242fb19d4f98088a53f581ecc5b1a22
allgg <49602399+allgg@users.noreply.github.com>
2019-08-01 18:33:54 +0800

    Update fxs-m-top.js

--- fxs-m-top.js
+++ fxs-m-top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://m.ydxtpower.com:168/ua80512/  \' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'https://m.zhihengbest.com:168/ua80512/\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
