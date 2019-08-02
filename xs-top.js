092dd9a0c566227fbe5efb2d9b70baf6cbe11ee9
allgg <49602399+allgg@users.noreply.github.com>
2019-08-02 10:23:40 +0800

    Update xs-top.js

--- xs-top.js
+++ xs-top.js
@@ -1,1 +1,2 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
+document.writeln("<a href=\'https://you.seyuniu.xyz\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
