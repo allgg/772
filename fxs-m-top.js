ed5b78ddc38f3814a4d19790946553de0ad5a98b
allgg <49602399+allgg@users.noreply.github.com>
2019-08-02 10:22:36 +0800

    Update fxs-m-top.js

--- fxs-m-top.js
+++ fxs-m-top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://m.zhihengbest.com:168/ua80512/\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'https://you.seyuniu.xyz\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
