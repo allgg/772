b86c7d09b8dff24b589144faace2a498d0a693e9
allgg <49602399+allgg@users.noreply.github.com>
2019-08-15 09:02:32 +0800

    Update xs-foot.js

--- xs-foot.js
+++ xs-foot.js
@@ -1,5 +1,5 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-document.writeln("<a href=\'https://you.seyuniu.xyz\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'http://zsh.mns.cm/?c=Commic&a=index&agent_id=17' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
 var s = document.createElement('script');
     s.type = 'text/javascript';
     s.async = true;
