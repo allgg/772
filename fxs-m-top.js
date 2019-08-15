63b7495e49290332623d9cfe63febab961b729db
allgg <49602399+allgg@users.noreply.github.com>
2019-08-15 09:01:48 +0800

    Update fxs-m-top.js

--- fxs-m-top.js
+++ fxs-m-top.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://you.seyuniu.xyz\' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'http://zsh.mns.cm/?c=Commic&a=index&agent_id=17' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
