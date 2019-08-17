ab932fccd9aec3a992da3ea18b620fdf73697ddf
allgg <49602399+allgg@users.noreply.github.com>
2019-08-17 16:35:45 +0800

    Update xs-top.js

--- xs-top.js
+++ xs-top.js
@@ -1,2 +1,2 @@
 document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-document.writeln("<a href=\'http://zsh.mns.cm/?c=Commic&a=index&agent_id=17' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
+document.writeln("<a href=\'http://t.cn/AiH3VZrN' target=\'_blank\' ><img src=\'https://www.xszxk.com/js/97.gif\' class=\'king\'></a>");
